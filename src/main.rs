5df834958822c46e40ac77306acb05597713e477
Konrad Borowski <konrad@borowski.pw>
2019-03-04 14:10:08 +0100

    Revert "Enable Cloudflare Rocket loader"
    This reverts commit 944c15ba4ce63021d51221ce52fae163211bd9b7.

--- src/main.rs
+++ src/main.rs
@@ -199,7 +199,7 @@ fn main() -> io::Result<()> {
                         CONTENT_SECURITY_POLICY,
                         concat!(
                             "default-src 'none'; ",
-                            "script-src 'self' ajax.cloudflare.com; ",
+                            "script-src 'self'; ",
                             "style-src 'self'; ",
                             "img-src 'self'; ",
                             "object-src 'none'; ",
