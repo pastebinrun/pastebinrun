6964695819eeffda1f96ba0cf2f5474a313c9878
Konrad Borowski <konrad@borowski.pw>
2019-08-09 09:52:38 +0200

    Use the connection pool for migrations

--- src/main.rs
+++ src/main.rs
@@ -15,10 +15,10 @@ type PgPool = Pool<ConnectionManager<PgConnection>>;
 fn main() -> Result<(), Box<dyn Error>> {
     env_logger::init();
     let database_url = env::var("DATABASE_URL").expect("DATABASE_URL required");
-    diesel_migrations::run_pending_migrations(&PgConnection::establish(&database_url)?)?;
     let pool = &*Box::leak(Box::new(
         Pool::new(ConnectionManager::new(database_url)).expect("Couldn't create a connection pool"),
     ));
+    diesel_migrations::run_pending_migrations(&pool.get()?)?;
     warp::serve(routes::routes(pool)).run(([127, 0, 0, 1], 8080));
     Ok(())
 }
