80ca31bcbe9bf11f9cfede4e7b10e55225f7ea60
Konrad Borowski <konrad@borowski.pw>
2019-09-19 23:43:40 +0200

    Add a basic test for password validator

--- src/models/register.rs
+++ src/models/register.rs
@@ -1,6 +1,6 @@
 use crate::schema::users;
-use crate::Connection;
 use diesel::dsl::{exists, select};
+use diesel::pg::Pg;
 use diesel::prelude::*;
 use serde::Deserialize;
 use std::fmt::{self, Display, Formatter};
@@ -14,37 +14,40 @@ pub struct Form {
 }
 
 impl Form {
-    pub fn validate(&self, connection: &Connection) -> Result<Vec<Issue>, Rejection> {
+    pub fn validate(
+        &self,
+        connection: &impl Connection<Backend = Pg>,
+    ) -> Result<Vec<Issue>, Rejection> {
         let mut issues = Vec::new();
         let nickname = self.nickname.trim();
         let password = self.password.trim();
         let confirm_password = self.confirm_password.trim();
         if nickname.is_empty() {
             issues.push(Issue::MissingNickname);
-        } else {
-            if select(exists(users::table.filter(users::nickname.eq(nickname))))
-                .get_result(connection)
-                .map_err(warp::reject::custom)?
-            {
-                issues.push(Issue::NicknameAlreadyUsed)
-            }
+        } else if select(exists(users::table.filter(users::nickname.eq(nickname))))
+            .get_result(connection)
+            .map_err(warp::reject::custom)?
+        {
+            issues.push(Issue::NicknameAlreadyUsed)
         }
-        let empty_password = password.is_empty();
-        if empty_password {
+        if password.is_empty() {
             issues.push(Issue::MissingPassword);
-        } else if password.len() < 8 {
-            // Yes, I'm checking byte length, that's intentional
-            issues.push(Issue::PasswordShorterThanEightCharacters);
-        }
-        if confirm_password.is_empty() {
-            issues.push(Issue::MissingConfirmPassword);
-        } else if !empty_password && password != confirm_password {
-            issues.push(Issue::PasswordsNotTheSame);
+        } else {
+            if password.len() < 8 {
+                // Yes, I'm checking byte length, that's intentional
+                issues.push(Issue::PasswordShorterThanEightCharacters);
+            }
+            if confirm_password.is_empty() {
+                issues.push(Issue::MissingConfirmPassword);
+            } else if password != confirm_password {
+                issues.push(Issue::PasswordsNotTheSame);
+            }
         }
         Ok(issues)
     }
 }
 
+#[derive(Debug, Eq, PartialEq)]
 pub enum Issue {
     MissingNickname,
     NicknameAlreadyUsed,
@@ -65,5 +68,21 @@ impl Display for Issue {
             Self::PasswordsNotTheSame => "Passwords must be identical",
         };
         write!(f, "{}", message)
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::{Form, Issue};
+    use diesel::prelude::*;
+    use std::env;
+
+    #[test]
+    fn empty_everything_report() {
+        let connection = PgConnection::establish(&env::var("DATABASE_URL").unwrap()).unwrap();
+        assert_eq!(
+            Form::default().validate(&connection).unwrap(),
+            &[Issue::MissingNickname, Issue::MissingPassword],
+        )
     }
 }
