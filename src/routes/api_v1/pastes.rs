ef5652bc1d4eb2a585ea0a0f4204425c2238d01e
Konrad Borowski <konrad@borowski.pw>
2019-11-03 19:13:55 +0100

    Use durations instead of timestamps in API
    Most users will want to specify concrete expiration duration, and using
    timestamps would necessitate doing math, when it would be easier if
    the API itself did the math.

--- src/routes/api_v1/pastes.rs
+++ src/routes/api_v1/pastes.rs
@@ -1,19 +1,54 @@
 use crate::models::paste;
 use crate::Connection;
-use chrono::{DateTime, Utc};
+use chrono::{Duration, Utc};
 use futures::Future;
 use futures03::TryFutureExt;
-use serde::Deserialize;
+use serde::de::{Deserializer, Unexpected, Visitor};
+use serde::{de, Deserialize};
+use std::fmt::{self, Formatter};
 use tokio_executor::blocking;
 use warp::Rejection;
 
 #[derive(Deserialize)]
 #[serde(deny_unknown_fields)]
 pub struct PasteForm {
-    delete_at: Option<DateTime<Utc>>,
+    expiration: Option<DeserializableDuration>,
     #[serde(default = "default_language")]
     language: String,
     code: String,
+}
+
+struct DeserializableDuration(Duration);
+
+impl<'de> Deserialize<'de> for DeserializableDuration {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        struct DurationVisitor;
+
+        impl<'de> Visitor<'de> for DurationVisitor {
+            type Value = DeserializableDuration;
+
+            fn expecting(&self, f: &mut Formatter<'_>) -> fmt::Result {
+                write!(f, "an RFC 3339 duration string")
+            }
+
+            fn visit_str<E>(self, input: &str) -> Result<DeserializableDuration, E>
+            where
+                E: de::Error,
+            {
+                let parsed = time_parse::duration::parse_nom(input).map_err(|_| {
+                    E::invalid_value(Unexpected::Str(input), &"an RFC 3339 duration")
+                })?;
+                Ok(DeserializableDuration(
+                    Duration::from_std(parsed).map_err(E::custom)?,
+                ))
+            }
+        }
+
+        deserializer.deserialize_str(DurationVisitor)
+    }
 }
 
 fn default_language() -> String {
@@ -22,11 +57,19 @@ fn default_language() -> String {
 
 pub fn insert_paste(
     PasteForm {
-        delete_at,
+        expiration,
         language,
         code,
     }: PasteForm,
     connection: Connection,
 ) -> impl Future<Item = String, Error = Rejection> {
-    blocking::run(move || paste::insert(&connection, delete_at, &language, code)).compat()
+    blocking::run(move || {
+        paste::insert(
+            &connection,
+            expiration.map(|expiration| Utc::now() + expiration.0),
+            &language,
+            code,
+        )
+    })
+    .compat()
 }
