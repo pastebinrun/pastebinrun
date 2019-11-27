a8ee83739de8c07cf69c6c7cf51c30801749b247
Konrad Borowski <konrad@borowski.pw>
2019-11-27 19:16:03 +0100

    Add source code link

--- js/views/config-link.ts
+++ js/views/config-link.ts
@@ -1,10 +1,8 @@
 export default function addOptionsLink() {
-    const ul = document.createElement('ul')
     const li = document.createElement('li')
     const a = document.createElement('a')
     a.textContent = 'Config'
     a.href = '/config'
     li.append(a)
-    ul.append(li)
-    document.getElementById('menu-buttons').append(ul)
+    document.querySelector('#menu-buttons ul').append(li)
 }
