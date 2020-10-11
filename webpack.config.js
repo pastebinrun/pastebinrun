83c577bfa9d2296a09e6000870ad578e8b38ba58
Konrad Borowski <konrad@borowski.pw>
2020-10-11 19:21:22 +0200

    Use local prism.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,44 @@
 const { StatsWriterPlugin } = require("webpack-stats-plugin")
+const glob = require('glob')
+const path = require('path')
 
-module.exports = {
-  entry: './js/index',
-  output: {
-    filename: 'static/js/[chunkhash].js',
-    path: __dirname,
-  },
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: 'ts-loader',
-      },
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
-      },
+module.exports = [
+  {
+    entry: './js/index',
+    output: {
+      filename: 'static/js/[chunkhash].js',
+      path: __dirname,
+    },
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          use: 'ts-loader',
+        },
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+      ],
+    },
+    resolve: {
+      extensions: ['.ts', '.js'],
+    },
+    plugins: [
+      new StatsWriterPlugin({
+        filename: 'entry',
+        transform(data) {
+          return data.assetsByChunkName.main[0]
+        },
+      }),
     ],
   },
-  resolve: {
-    extensions: ['.ts', '.js'],
+  {
+    entry: Object.fromEntries(glob.sync('./node_modules/prismjs/components/*.min.js')
+      .map(p => [path.basename(p), p])),
+    output: {
+      filename: 'static/js/[name]',
+      path: __dirname,
+    }
   },
-  plugins: [
-    new StatsWriterPlugin({
-      filename: 'entry',
-      transform(data) {
-        return data.assetsByChunkName.main[0]
-      },
-    }),
-  ],
-}
+]
