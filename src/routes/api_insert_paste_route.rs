3f62644ffe5a3a0a091473be38e4a991d48cac36
Konrad Borowski <konrad@borowski.pw>
2021-10-19 14:44:56 +0200

    Enable CORS for inserting pastes using API

--- src/routes/api_insert_paste_route.rs
+++ src/routes/api_insert_paste_route.rs
@@ -3,6 +3,10 @@ use crate::Db;
 use chrono::Duration;
 use chrono::Utc;
 use rocket::form::{self, Form, FromFormField, Strict, ValueField};
+use rocket::http::hyper::header::ACCESS_CONTROL_ALLOW_ORIGIN;
+use rocket::http::Header;
+use rocket::request::Request;
+use rocket::response::{self, Responder, Response};
 use std::error::Error;
 
 #[derive(FromForm)]
@@ -25,24 +29,37 @@ impl<'r> FromFormField<'r> for Expiration {
     }
 }
 
+pub struct CorsString(String);
+
+impl<'r> Responder<'r, 'static> for CorsString {
+    fn respond_to(self, r: &Request<'_>) -> response::Result<'static> {
+        Response::build()
+            .merge(self.0.respond_to(r)?)
+            .header(Header::new(ACCESS_CONTROL_ALLOW_ORIGIN.as_str(), "*"))
+            .ok()
+    }
+}
+
 #[post("/api/v1/pastes", data = "<form>")]
 pub async fn api_insert_paste(
     db: Db,
     form: Form<Strict<PasteForm>>,
-) -> Result<String, InsertionError> {
-    db.run(move |conn| {
-        paste::insert(
-            conn,
-            form.expiration.0.map(|expiration| Utc::now() + expiration),
-            &form.language,
-            &form.code,
-            ExtraPasteParameters {
-                stdin: "",
-                stdout: None,
-                stderr: None,
-                exit_code: None,
-            },
-        )
-    })
-    .await
+) -> Result<CorsString, InsertionError> {
+    let identifier = db
+        .run(move |conn| {
+            paste::insert(
+                conn,
+                form.expiration.0.map(|expiration| Utc::now() + expiration),
+                &form.language,
+                &form.code,
+                ExtraPasteParameters {
+                    stdin: "",
+                    stdout: None,
+                    stderr: None,
+                    exit_code: None,
+                },
+            )
+        })
+        .await?;
+    Ok(CorsString(identifier))
 }
