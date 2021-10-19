0cc2c08e10e28b0ef46201b696032650355fd34e
Konrad Borowski <konrad@borowski.pw>
2021-10-19 14:27:43 +0200

    Enable CORS for raw pastes

--- src/routes/raw_paste_route.rs
+++ src/routes/raw_paste_route.rs
@@ -18,10 +18,12 @@ use crate::models::paste::Paste;
 use crate::schema::pastes;
 use crate::Db;
 use diesel::prelude::*;
-use rocket::http::impl_from_uri_param_identity;
+use rocket::http::hyper::header::ACCESS_CONTROL_ALLOW_ORIGIN;
 use rocket::http::uri::fmt::{Formatter, FromUriParam, Path, UriDisplay};
-use rocket::request::FromParam;
-use rocket::response::Debug;
+use rocket::http::{impl_from_uri_param_identity, Header};
+use rocket::request::{FromParam, Request};
+use rocket::response::status::NotFound;
+use rocket::response::{self, Debug, Responder, Response};
 use std::fmt;
 
 pub struct WithTxt(String);
@@ -55,18 +57,41 @@ impl FromUriParam<Path, String> for WithTxt {
     }
 }
 
+pub enum RawPasteResponse {
+    Found(String),
+    NotFound,
+}
+
+const PASTE_NOT_FOUND_MESSAGE: &str =
+    "404 Paste Not Found\n\nIt could have been automatically deleted after 24 hours.\n";
+
+impl<'r> Responder<'r, 'static> for RawPasteResponse {
+    fn respond_to(self, r: &Request<'_>) -> response::Result<'static> {
+        Response::build()
+            .merge(if let Self::Found(paste) = self {
+                paste.respond_to(r)
+            } else {
+                NotFound(PASTE_NOT_FOUND_MESSAGE).respond_to(r)
+            }?)
+            .header(Header::new(ACCESS_CONTROL_ALLOW_ORIGIN.as_str(), "*"))
+            .ok()
+    }
+}
+
 #[get("/<identifier>")]
 pub async fn raw_paste(
     db: Db,
     identifier: WithTxt,
-) -> Result<Option<String>, Debug<diesel::result::Error>> {
-    db.run(move |conn| {
-        Paste::delete_old(conn)?;
-        Ok(pastes::table
-            .select(pastes::paste)
-            .filter(pastes::identifier.eq(&identifier.0))
-            .get_result(conn)
-            .optional()?)
-    })
-    .await
+) -> Result<RawPasteResponse, Debug<diesel::result::Error>> {
+    Ok(db
+        .run(move |conn| {
+            Paste::delete_old(conn)?;
+            pastes::table
+                .select(pastes::paste)
+                .filter(pastes::identifier.eq(&identifier.0))
+                .get_result(conn)
+                .optional()
+        })
+        .await?
+        .map_or_else(|| RawPasteResponse::NotFound, RawPasteResponse::Found))
 }
