693d5545f7492af51110c5d820d3663cb9560842
Konrad Borowski <konrad.borowski@kontomatik.com>
2022-01-17 15:17:41 +0100

    Update copyright years for models/paste.rs

--- src/models/paste.rs
+++ src/models/paste.rs
@@ -1,5 +1,5 @@
 // pastebin.run
-// Copyright (C) 2020 Konrad Borowski
+// Copyright (C) 2020-2022 Konrad Borowski
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Affero General Public License as published by
