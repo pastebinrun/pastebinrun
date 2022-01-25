d1fc0e03e222f11cf8cb2c831c2ed65c7c607c92
Konrad Borowski <konrad.borowski@kontomatik.com>
2022-01-25 16:16:29 +0100

    Remove about pages migrations

--- migrations/2019-03-15-065751_nofollow-column/up.sql
+++ migrations/2019-03-15-065751_nofollow-column/up.sql
@@ -1,3 +1,1 @@
 ALTER TABLE pastes ADD COLUMN no_follow boolean NOT NULL DEFAULT TRUE;
-
-UPDATE pastes SET no_follow = FALSE WHERE identifier = 'about';
--- migrations/2019-04-08-075940_remove-about-page/up.sql
+++
@@ -1,1 +0,0 @@
-DELETE FROM pastes WHERE identifier = 'about';
