f6e118f3b69ab6de043d6ec3cb3a50e2f83071f0
Konrad Borowski <konrad@borowski.pw>
2022-05-06 20:03:28 +0200

    Rename "Config" to "Options"

--- js/views/config-link.ts
+++ js/views/config-link.ts
@@ -17,7 +17,7 @@
 export default function addOptionsLink() {
   const li = document.createElement("li");
   const a = document.createElement("a");
-  a.textContent = "Config";
+  a.textContent = "Options";
   a.href = "/config";
   li.append(a);
   document.querySelector("#menu-buttons ul").append(li);
