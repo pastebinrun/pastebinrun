314a5b7a9f4480d58acd70234e9610fcaea594ff
Konrad Borowski <konrad@borowski.pw>
2022-06-11 14:48:57 +0200

    Dynamically load highlighters in Markdown code blocks

---
+++ js/views/CodeMirrorEditor/markdown.ts
@@ -0,0 +1,15 @@
+import { markdown } from "@codemirror/lang-markdown";
+import { LanguageDescription } from "@codemirror/language";
+import { languages } from "@codemirror/language-data";
+
+export default markdown({
+  codeLanguages: languages.concat(
+    LanguageDescription.of({
+      name: "Nix",
+      extensions: ["nix"],
+      async load() {
+        return (await import("@replit/codemirror-lang-nix")).nix();
+      },
+    })
+  ),
+});
